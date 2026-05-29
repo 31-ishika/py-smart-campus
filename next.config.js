bc54bb2bc951625cbb0a13e3c8f6537dbcb19935
k_ishika <ishikakalawatiya@gmail.com>
2026-05-29 16:45:16 +0530

    Keep GitHub Pages deployment config updated

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/py-smart-campus',
+  assetPrefix: '/py-smart-campus',
+  output: 'export',
 }
 module.exports = nextConfig
